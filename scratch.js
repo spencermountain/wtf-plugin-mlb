1e6803438633d7046c3ac450f7a84896e48743a3
spencer kelly <spencermountain@gmail.com>
2018-09-01 12:47:08 -0400

    tests passing now

--- scratch.js
+++ scratch.js
@@ -1,10 +1,9 @@
 const wtfMLB = require('./src')
 // const wtf = require('wtf_wikipedia')
-
-wtfMLB.fetch('New York Mets', 2017).catch(console.log).then((data => {
-  console.log(data.slice(0, 5))
+//
+wtfMLB.fetch('Washington Nationals', 2017).catch(console.log).then((data => {
   console.log(data.length)
+  console.log(data.slice(166,data.length))
 }))
-
 
 //| 2 || April 2 || @ [[2014 New York Mets season|Mets]] || 5–1 || '''[[Gio González|González]]''' (1–0) || [[Bartolo Colón|Colón]] (0–1) || || 29,146 || 2–0
--- tests/games.test.js
+++ tests/games.test.js
@@ -7,9 +7,11 @@ test('Washington Nationals 2017', t => {
     t.notEqual(data[0].date, null, 'has game-0-date')
     // t.equal(data.length, 170, 'has 162 games') //+ rain outs?
     t.ok(data.length > 161, 'has atleast 161 games') //+ rain outs?
-    t.notEqual(data[data.length-1].Record, '97–65', 'got last record')
-    t.notEqual(data[data.length-1].Attendance, 35,652, 'got last attendance')
-    t.notEqual(data[data.length-1].Date, 'October 1', 'got last date')
+    t.equal(data[data.length-1].record.wins, 97, 'got last record wins')
+    t.equal(data[data.length-1].record.losses, 65, 'got last record losses')
+    t.equal(data[data.length-1].record.games, 162, 'got last record games')
+    t.equal(data[data.length-1].attendance, 35652, 'got last attendance')
+    t.equal(data[data.length-1].date, 'October 1', 'got last date')
     t.end();
   }))
 });
@@ -20,9 +22,22 @@ test('Blue Jays 2016', t => {
     t.equal(data[0].result.win, true, 'has game-0-win')
     // t.equal(data.length, 170, 'has 162 games') //+ rain outs?
     t.ok(data.length > 161, 'has atleast 161 games') //+ rain outs?
-    t.notEqual(data[data.length-1].Record, '89–73', 'got last record')
-    t.notEqual(data[data.length-1].Attendance, 36787, 'got last attendance')
-    t.notEqual(data[data.length-1].Date, 'October 2', 'got last date')
+    t.equal(data[data.length-1].record.wins, 89, 'got last record')
+    t.equal(data[data.length-1].record.losses, 73, 'got last record')
+    t.equal(data[data.length-1].attendance, 36787, 'got last attendance')
+    t.equal(data[data.length-1].date, 'October 2', 'got last date')
+    t.end();
+  }))
+});
+
+test('New York Mets 2017', t => {
+  wtfMlb.fetch('New York Mets', 2017).catch(console.log).then((data => {
+    t.equal(data[0].date, 'April 3', 'has game-0-date')
+    t.equal(data[0].result.win, true, 'has game-0-win')
+    t.equal(data[0].result.them, 0, 'has game-0-score')
+    t.equal(data[0].attendance, 44384, 'has game-0-attendance')
+    // t.equal(data.length, 170, 'has 162 games') //+ rain outs?
+    t.ok(data.length > 161, 'has atleast 161 games') //+ rain outs?
     t.end();
   }))
 });
